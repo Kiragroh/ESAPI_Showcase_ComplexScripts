c1dfd05301d2ffb462c31e0082bff52b1448bd10
Max Grohmann <44711812+Kiragroh@users.noreply.github.com>
2020-08-02 09:05:16 +0200

    Add files via upload

---
+++ shell.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using VMS.TPS.Common.Model.API;
+using VMS.TPS.Common.Model.Types;
+using System.Windows.Controls;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading.Tasks;
+
+//change version for possible new approval in clinical mode
+[assembly: AssemblyVersion("2.1")]
+[assembly: ESAPIScript(IsWriteable = true)]
+
+namespace VMS.TPS
+{
+
+    public class Script
+    {
+        //Add your code here
+    }
+
+
+}
